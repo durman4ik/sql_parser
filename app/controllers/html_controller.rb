5f43d29bbeccc389c64cabbe7b495fc5a5675c58
Yury Matusevich <yur4ik93@mail.ru>
2017-03-05 02:04:56 +0300

    add options to html2haml

--- app/controllers/html_controller.rb
+++ app/controllers/html_controller.rb
@@ -8,13 +8,32 @@ class HtmlController < ApplicationController
   def create
     @html = Html.new(html_params)
     @html.convert_to_haml
-    render 'index'
-  end
 
+    if html_params[:save_as].present?
+      save_file
+    else
+      render 'index'
+    end
+  end
 
   private
 
+  def save_file
+    if html_params[:save_as][:html]
+      send_file_to_user(@html.html, 'text/html', 'file_name.html')
+    else
+      send_file_to_user(@html.haml, 'text/haml', 'file_name.html.haml')
+    end
+  end
+
+  def send_file_to_user(file, type, filename)
+    send_data file,
+              type: type,
+              disposition: 'attachment',
+              filename: filename
+  end
+
   def html_params
-    params.require(:data).permit(:html, :haml, :old_ruby)
+    params.require(:data).permit(:html, :haml, :old_ruby, save_as: [:html, :haml])
   end
 end
